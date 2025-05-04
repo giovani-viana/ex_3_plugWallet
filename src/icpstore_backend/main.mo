a5fae9af21924d04986472c54090cf4839f72532
diogodcmdcm <diogo.micheluzzi@gmail.com>
2025-05-03 22:10:42 -0300

    Adicionado o projeto

--- src/icpstore_backend/main.mo
+++ src/icpstore_backend/main.mo
@@ -1,5 +1,34 @@
+import Array "mo:base/Array";
+
 actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+  
+  type Product = {
+    title: Text;
+    description: Text;
+    image: Text;    
+  };    
+  
+  public query func getProducts() : async [Product] {    
+      
+      var products : [Product] = []; 
+
+      let p1 : Product = { title = "Motoko para Iniciantes"; description = "Aprenda os fundamentos da linguagem Motoko e como criar DApps na ICP."; image = "https://via.placeholder.com/300x180?text=Motoko"};
+      let p2 : Product = { title = "Tokens ICRC-1 e ICRC-2"; description = "Descubra como criar e gerenciar tokens na Internet Computer."; image = "https://via.placeholder.com/300x180?text=ICRC"};
+      let p3 : Product = { title = "Front-end com React na ICP"; description = "Construa interfaces modernas para DApps usando React na ICP."; image = "https://via.placeholder.com/300x180?text=React"};
+      let p4 : Product = { title = "Canisters e Deploys"; description = "Entenda o ciclo de vida dos canisters e como fazer deploy na ICP."; image = "https://via.placeholder.com/300x180?text=Canisters" };
+      let p5 : Product = { title = "Web3 e Identidade Descentralizada"; description = "Explore conceitos de identidade na Web3 e autenticação na ICP."; image = "https://via.placeholder.com/300x180?text=Web3" };
+      let p6 : Product = { title = "NFTs na ICP"; description = "Aprenda como criar, mintar e vender NFTs usando a Internet Computer."; image = "https://via.placeholder.com/300x180?text=NFTs"};
+
+      products := Array.append(products, [p1]);
+      products := Array.append(products, [p2]);
+      products := Array.append(products, [p3]);
+      products := Array.append(products, [p4]);
+      products := Array.append(products, [p5]);
+      products := Array.append(products, [p6]);
+
+      return products;
   };
+
+
+
 };
--- src/icpstore_frontend/src/App.jsx
+++ src/icpstore_frontend/src/App.jsx
@@ -1,30 +1,38 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { icpstore_backend } from 'declarations/icpstore_backend';
+import { Principal } from '@dfinity/principal'; 
+import { idlFactory } from '../../declarations/icpsc_icrc1_ledger_canister/icpsc_icrc1_ledger_canister.did.js';
 
 function App() {
-  const [greeting, setGreeting] = useState('');
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    icpstore_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
+  //obtem o id do canister da ledger do token ICPSC
+  const canisterLedgerId = process.env.CANISTER_ID_ICPSC_ICRC1_LEDGER_CANISTER;
+  // Quando for desenvolvimento local a variavel process.env.DFX_NETWORK irá retornar a string 'local'
+  const host = process.env.DFX_NETWORK === 'ic' ? 'https://mainnet.dfinity.network' : 'http://localhost:4943';
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {    
+    const init = async () => {
+      setProducts(await icpstore_backend.getProducts());
+    }
+    init();
+  }, []);  
 
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
+    <div className="container">
+      <h1 className="titulo">Loja de Cursos da ICP</h1>
+      <div className="grade-cursos">
+        {products.map((p, index) => (
+          <div key={index} className="card">
+            <img src={p.image} alt={p.title} className="imagem" />
+            <h2 className="card-titulo">{p.title}</h2>
+            <p className="descricao">{p.description}</p>
+            <button className="botao">Comprar</button>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
